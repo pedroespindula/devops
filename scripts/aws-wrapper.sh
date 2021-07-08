5ddf161a5be26b46bdfe51283c2db96376cdd670
Pedro Espíndula <joaopedro.jpse@gmail.com>
2021-07-08 12:02:06 -0300

    Add stop running instances function

--- scripts/aws-wrapper.sh
+++ scripts/aws-wrapper.sh
@@ -1,7 +1,7 @@
 OWNER=pedro
 
 aws-ec2-ls() {
-  aws ec2 describe-instances --query "Reservations[].Instances[].{Name: Tags[?Key=='Name']|[0].Value, Id: InstanceId, Status: State.Name, Ip: NetworkInterfaces[0].PrivateIpAddress, LastStarted: LaunchTime, Tags: Tags}" "$@"
+  aws ec2 describe-instances --query "Reservations[].Instances[].{Name: Tags[?Key=='Name']|[0].Value, Id: InstanceId, Status: State.Name, PrivateIp: NetworkInterfaces[0].PrivateIpAddress, PublicIp: NetworkInterfaces[0].PrivateIpAddresses[0].Association.PublicIp, LastStarted: LaunchTime, Tags: Tags}" "$@"
 }
 
 aws-ec2-ls-all() {
@@ -12,8 +12,12 @@ aws-ec2-ls-running() {
   aws-ec2-ls --filters Name=instance-state-name,Values=running
 }
 
+aws-ec2-ls-running-ids() {
+  aws-ec2-ls-running | jq --raw-output '.[].Id' | tr "\n" " "
+}
+
 aws-ec2-ls-user() {
-  aws-ec2-ls --filters Name=tag:Owner,Values=$1
+  aws-ec2-ls --filters Name=tag:User,Values=$1
 }
 
 aws-ec2-ls-mine() {
@@ -30,6 +34,10 @@ aws-ec2-start() {
 
 aws-ec2-stop() {
   aws ec2 stop-instances --instance-ids "$@"
+}
+
+aws-ec2-stop-running() {
+  aws-ec2-stop $(aws-ec2-ls-running-ids)
 }
 
 aws-ec2-start-mine() {
