67f0c83be0cfe0293cdc02cc41cc69f246c72b00
Pedro Espíndula <joaopedro.jpse@gmail.com>
2021-07-11 10:32:04 -0300

    Add optional args on every function

--- scripts/aws-wrapper.sh
+++ scripts/aws-wrapper.sh
@@ -9,23 +9,31 @@ aws-ec2-ls-all() {
 }
 
 aws-ec2-ls-running() {
-  aws-ec2-ls --filters Name=instance-state-name,Values=running
+  aws-ec2-ls --filters Name=instance-state-name,Values=running "$@" 
+}
+
+aws-ec2-ls-mine-running() {
+  aws-ec2-ls-mine --filters Name=instance-state-name,Values=running "$@" 
 }
 
 aws-ec2-ls-running-ids() {
-  aws-ec2-ls-running | jq --raw-output '.[].Id' | tr "\n" " "
+  aws-ec2-ls-running "$@" | jq --raw-output '.[].Id' | tr "\n" " "
+}
+
+aws-ec2-ls-mine-running-ids() {
+  aws-ec2-ls-mine-running "$@" | jq --raw-output '.[].Id' | tr "\n" " "
 }
 
 aws-ec2-ls-user() {
-  aws-ec2-ls --filters Name=tag:User,Values=$1
+  aws-ec2-ls --filters Name=tag:User,Values="$@" 
 }
 
 aws-ec2-ls-mine() {
-  aws-ec2-ls-user $OWNER
+  aws-ec2-ls-user $OWNER "$@" 
 }
 
 aws-ec2-ls-mine-ids() {
-  aws-ec2-ls-mine | jq --raw-output '.[].Id' | tr "\n" " "
+  aws-ec2-ls-mine "$@" | jq --raw-output '.[].Id' | tr "\n" " "
 }
 
 aws-ec2-start() {
@@ -37,13 +45,17 @@ aws-ec2-stop() {
 }
 
 aws-ec2-stop-running() {
-  aws-ec2-stop $(aws-ec2-ls-running-ids)
+  aws-ec2-stop $(aws-ec2-ls-running-ids) "$@" 
+}
+
+aws-ec2-stop-mine-running() {
+  aws-ec2-stop $(aws-ec2-ls-mine-running-ids) "$@" 
 }
 
 aws-ec2-start-mine() {
-  aws-ec2-start $(aws-ec2-ls-mine-ids)
+  aws-ec2-start $(aws-ec2-ls-mine-ids) "$@" 
 }
 
 aws-ec2-stop-mine() {
-  aws-ec2-stop $(aws-ec2-ls-mine-ids)
+  aws-ec2-stop $(aws-ec2-ls-mine-ids) "$@" 
 }
